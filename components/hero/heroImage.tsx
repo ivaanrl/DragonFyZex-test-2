30bcf7c0fa7016bde5ad4b128d2e463abf05d893
ivaanrl <ivanrl2010@gmail.com>
2020-12-28 18:54:07 -0300

    remove heroImage styledcomponents

--- components/hero/heroImage.tsx
+++ components/hero/heroImage.tsx
@@ -1,15 +1,16 @@
 import { ImageContainer, NextImage } from './heroStyles';
-
+import Image from 'next/image';
 const HeroImage = () => {
   return (
-    <ImageContainer>
-      <NextImage
+    <div className="hero-image-container">
+      <Image
         width={1801}
         height={933}
         src="/images/hero/firstImage.png"
         alt="placeholder image"
+        className="hero-image"
       />
-    </ImageContainer>
+    </div>
   );
 };
 
