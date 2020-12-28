f82f20958320e3ea18a1043ef9fd14e677cd95ce
ivaanrl <ivanrl2010@gmail.com>
2020-12-28 18:50:15 -0300

    try if removing stuff from document.tsx fixes issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,23 +28,4 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="shortcut icon" href="images/Skupos/favicon.png" />
-          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;1,600&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
 }
