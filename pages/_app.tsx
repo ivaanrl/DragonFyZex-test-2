39ab2d83ba07e63f455c0d17356d909d08e839eb
ivaanrl <ivanrl2010@gmail.com>
2020-12-28 17:10:51 -0300

    lazy load sidebar and mobileMenu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
-import MobileMenu from '../components/navbar/mobileMenu';
+//import MobileMenu from '../components/navbar/mobileMenu';
 import Navbar from '../containers/navbar/navbar';
 import '../styles/globals.css';
 import { theme } from '../styles/theme';
 import { ThemeProvider } from 'styled-components';
 import Footer from '../components/footer/footer';
+import loadable from '@loadable/component';
+
+const MobileMenu = loadable(() => import('../components/navbar/mobileMenu'));
 
 function MyApp({ Component, pageProps }) {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
